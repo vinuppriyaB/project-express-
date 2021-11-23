c9f2ed28894bb422f324de0f772198f1de0a08ba
vinuppriya balachandran <vinuppriya.b@gmail.com>
2021-11-23 09:35:43 +0530

    mongo atlaspostman

--- index.js
+++ index.js
@@ -1,79 +1,171 @@
-const movies=[
-    {
-            id:"100",
-            name: "Ratatouille",
-            pic: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/2b18044415b238c004e702aaf1dc693daf886d9dadf4b78e1b1bcc0b9df10f8c._RI_V_TTW_.jpg",
-            summary:
-              "Remy dreams of becoming a great chef, despite being a rat in a definitely rodent-phobic profession. He moves to Paris to follow his dream, and with the help of hapless garbage boy Linguini he puts his culinary skills to the test in the kitchen but he has to stay in hiding at the same time, with hilarious consequences. Remy eventually gets the chance to prove his culinary abilities to a great food critic but is the food good? A Pixar animation.",
-            rating: "8 ",
-            trailer: "https://www.youtube.com/embed/NgsQ8mVkN8w",
-            language:"english",
-          },
-          {
-            id:"101",
-            name: "Alita:Battle Angle",
-            pic: "https://proseandcomiccons.files.wordpress.com/2019/02/slick-new-poster-for-alita-battle-angel1.jpg?w=640",
-            summary:
-              "Set several centuries in the future, the abandoned Alita is found in the scrapyard of Iron City by Ido, a compassionate cyber-doctor who takes the unconscious cyborg Alita to his clinic. When Alita awakens, she has no memory of who she is, nor does she have any recognition of the world she finds herself in. As Alita learns to navigate her new life and the treacherous streets of Iron City, Ido tries to shield her from her mysterious past.",
-            rating: " 7.7",
-            trailer: "https://www.youtube.com/embed/cislZ9S0ocA",
-            language:"english",
-          },
-          
-          {
-            id:"102",
-            name: "City Hunter",
-            pic: "https://static.xemovie.com/images/Film/Series/City%20hunter.jpg",
-            summary:
-              "In 1983, the South Korean president and his delegates are visiting Burma when a bomb planted by North Korean agents explodes, killing some high-ranking officials. This historical event is called the Rangoon bombing (also known as the Rangoon incident).",
-            rating: "8.1",
-            trailer: "https://www.youtube.com/embed/YGr706kT7SM",
-            language:"korean",
-          },
-          {
-            id:"103",
-            name: " Interstellar",
-            pic: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
-      
-            summary:
-              "In Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth's population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind's new home.",
-            rating: " 8.7",
-            trailer: "https://www.youtube.com/embed/zSWdZVtXT7E",
-            language:"english",
-          }
-]
+const movies = [
+  {
+    id: "100",
+    name: "Ratatouille",
+    pic: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/2b18044415b238c004e702aaf1dc693daf886d9dadf4b78e1b1bcc0b9df10f8c._RI_V_TTW_.jpg",
+    summary:
+      "Remy dreams of becoming a great chef, despite being a rat in a definitely rodent-phobic profession. He moves to Paris to follow his dream, and with the help of hapless garbage boy Linguini he puts his culinary skills to the test in the kitchen but he has to stay in hiding at the same time, with hilarious consequences. Remy eventually gets the chance to prove his culinary abilities to a great food critic but is the food good? A Pixar animation.",
+    rating: "8",
+    trailer: "https://www.youtube.com/embed/NgsQ8mVkN8w",
+    language: "english",
+  },
+  {
+    id: "101",
+    name: "Alita:Battle Angle",
+    pic: "https://proseandcomiccons.files.wordpress.com/2019/02/slick-new-poster-for-alita-battle-angel1.jpg?w=640",
+    summary:
+      "Set several centuries in the future, the abandoned Alita is found in the scrapyard of Iron City by Ido, a compassionate cyber-doctor who takes the unconscious cyborg Alita to his clinic. When Alita awakens, she has no memory of who she is, nor does she have any recognition of the world she finds herself in. As Alita learns to navigate her new life and the treacherous streets of Iron City, Ido tries to shield her from her mysterious past.",
+    rating: "7.7",
+    trailer: "https://www.youtube.com/embed/cislZ9S0ocA",
+    language: "english",
+  },
+
+  {
+    id: "102",
+    name: "City Hunter",
+    pic: "https://static.xemovie.com/images/Film/Series/City%20hunter.jpg",
+    summary:
+      "In 1983, the South Korean president and his delegates are visiting Burma when a bomb planted by North Korean agents explodes, killing some high-ranking officials. This historical event is called the Rangoon bombing (also known as the Rangoon incident).",
+    rating: "8.1",
+    trailer: "https://www.youtube.com/embed/YGr706kT7SM",
+    language: "korean",
+  },
+  {
+    id: "103",
+    name: " Interstellar",
+    pic: "https://m.media-amazon.com/images/I/A1JVqNMI7UL._SL1500_.jpg",
+
+    summary:
+      "In Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth's population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind's new home.",
+    rating: "8.7",
+    trailer: "https://www.youtube.com/embed/zSWdZVtXT7E",
+    language: "english",
+  },
+];
 // const express =require("express");
 import express from "express";
-const app=express();
-const PORT=9000;
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-app.get("/movies",(request,response)=>{
-  console.log(request.query);
-  const {language}=request.query;
-//   console.log(rating , typeof(rating));
-if(language)
-{
-    const result= movies.filter((mvn)=> mvn.language===language);
-    response.send(result);
+dotenv.config();
 
+const app = express();
+const PORT = 9000;
+const MONGO_URL = process.env.MONGO_URL;
+// mongodb+srv://vinuppriya:<password>@cluster0.xu3bs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+app.use(express.json());
+
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongodb connected");
+  
+  return client;
 }
- else{
-    response.send(movies);
+createConnection();
 
- }
- 
+app.get("/movies", async(request, response) => {
+  console.log(request.query);
+  let filter =request.query;
+// if(filter.rating)
+// {
+//     filter.rating=parseInt(filter.rating);
+// }
+  const client = await createConnection();
+  const movie = await client
+    .db("B27rwd")
+    .collection("movies")
+    .find(filter)
+    .toArray();
+//   const { language, rating } = request.query;
+//   let filtermovie = movies;
+
+//   if (language) {
+//     filtermovie = filtermovie.filter((mvn) => mvn.language === language);
+//   }
+//   if (rating) {
+//     filtermovie = filtermovie.filter((mvn) => mvn.rating === rating);
+//   }
+
+  response.send(movie);
+});
+
+app.get("/movies/:id", async (request, response) => {
+  const { id } = request.params;
+  const client = await createConnection();
+  const movie = await client
+    .db("B27rwd")
+    .collection("movies")
+    .findOne({ id: id });
+  console.log(movie);
+  
+  //    const result= movies.find((mvn)=> mvn.id===id);
+  //     result? response.send(result):response.send({message:"no match found"});
+  // response.send(result|| {message:"no match movie"})
+  response.send(movie);
+});
+app.get("/", (request, response) => {
+  response.send("welcome");
+});
+
+app.post("/movies", async (request, response) => {
+  const data = request.body;
+  const client = await createConnection();
+  console.log(data);
+  const movie = await client
+    .db("B27rwd")
+    .collection("movies")
+    .insertMany(data);
+  console.log(movie);
+
+  response.send(movie);
+});
+
+app.get("/movies/:id", async (request, response) => {
+  const { id } = request.params;
+  const client = await createConnection();
+  const result = await client
+    .db("b27rwd")
+    .collection("movies")
+    .findOne({ id: "102" });
+//   console.log(movie);
+  //    const result= movies.find((mvn)=> mvn.id===id);
+  //     result? response.send(result):response.send({message:"no match found"});
+  // response.send(result|| {message:"no match movie"})
+  response.send(result);
+});
+app.get("/", (request, response) => {
+  response.send("welcome");
+});
+
+app.delete("/movies/:id", async (request, response) => {
+    const { id } = request.params;
+    const client = await createConnection();
+    const movie = await client
+      .db("B27rwd")
+      .collection("movies")
+      .deleteOne({ id: id });
+    console.log(movie);
+    
+    
+    response.send(movie);
+  });
+  app.get("/", (request, response) => {
+    response.send("welcome");
+  });
+  app.delete("/movies", async(request, response) => {
+    console.log(request.query);
+    let filter =request.query;
+  
+    const client = await createConnection();
+    const movie = await client
+      .db("B27rwd")
+      .collection("movies")
+      .delete(filter)
+      
+  
+    response.send(movie);
+  });
   
 
-})
-
-
-app.get("/movies/:id",(request,response)=>{
-    const {id} = request.params;
-   const result= movies.find((mvn)=> mvn.id===id);
-//     result? response.send(result):response.send({message:"no match found"});
-    // response.send(result|| {message:"no match movie"})
-response.send(result);
-
-})
-
-app.listen(PORT,()=>console.log("the server is startedin",PORT));
+app.listen(PORT, () => console.log("the server is startedin", PORT));
